db8daa0ff893aba6ea73f9a76b860307695a937c
sourabhsjadhav99 <sourabhsjadhav99@gmail.com>
2024-10-22 00:07:33 +0530

    navbar completed with reponsiveness and home page in process

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import navdata from "../utils/navbarData";
 import { useRef, useState } from "react";
 import { IoMenu, IoClose } from "react-icons/io5";
 import useClickOutside from "../hooks/useClickOutside";
+import mindlogo from "../assets/images/mind_logo.png";
 function Header() {
   const location = useLocation();
   const [isOpenNavLinks, setIsOpenNavLinks] = useState(false);
@@ -18,17 +19,20 @@ function Header() {
   };
 
   return (
-    <div className="w-full  mb-[60px]">
-      <nav className="bg-white fixed top-0 left-0 w-full h-[60px] flex items-center justify-center px-2 border-b border-gray-300 ">
+    <div className="relative w-full ">
+      <nav className="bg-white w-full h-[90px] flex items-center justify-center px-2">
         <div className="w-[100%] md:w-[95%] lg:w-[90%] xl:w-[80%] h-full flex justify-between items-center ">
-          <div className="min-w-[10%] h-full flex justify-center items-center ">
-            <h3 className="text-2xl font-bold h-[80%] text-[#4cd681] ">
-              Mindmed Innovations
+          <div className="min-w-[10%] h-full flex justify-center items-center gap-2">
+            <div className="flex justify-center items-center">
+              <img src={mindlogo} alt="" className="h-[40px] w-[40px]" />{" "}
+            </div>
+            <h3 className="text-3xl font-bold h-[80%] hidden md:flex justify-center items-center">
+             <span className="text-secondary">Mindmed </span> <span className="text-ternary">Innovations</span>
             </h3>
           </div>
 
           {/* Middle Links (Hidden on small screens) */}
-          <div className="min-w-[30%]  hidden md:flex gap-3 md:gap-5 h-full justify-start">
+          <div className="min-w-[30%]  hidden md:flex gap-3 md:gap-5 h-[60%] justify-start">
             {navdata.map((link) => {
               return (
                 <NavLink
@@ -44,7 +48,7 @@ function Header() {
                       (link.path === "/" && isHomeActive) || isActive;
                     return `font-semibold h-full flex items-center transition-colors duration-300 ease-in-out  ${
                       applyActiveClass
-                        ? "text-[#4cd681] border-b-4 border-[#4cd681] font-bold"
+                        ? "text-ternary border-b-4 border-ternary font-bold"
                         : "text-gray-500 border-b-4 border-transparent hover:border-gray-200"
                     }`;
                   }}
@@ -59,6 +63,7 @@ function Header() {
           </button>
         </div>
       </nav>
+      <div className="absolute left-0 bottom-0 w-full h-[7px] bg-gradient-to-r from-secondary  to-[#00d258]"></div>
       {/* Mobile Menu */}
       <div
         className={`fixed flex md:hidden flex-col w-full inset-0 bg-white text-gray-700 z-50 transform transition-transform duration-500 ease-in-out p-5 ${
@@ -88,7 +93,7 @@ function Header() {
                       (link.path === "/" && isHomeActive) || isActive;
                     return `font-semibold h-full flex items-center transition-colors duration-300 ease-in-out  ${
                       applyActiveClass
-                        ? "text-[#4cd681]  font-bold"
+                        ? "text-ternary  font-bold"
                         : "text-gray-500 border-transparent hover:border-gray-200"
                     }`;
                   }}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,61 +1,106 @@
 // Home.js
-import React from "react";
 import { homeContent } from "../utils/homeContent"; // Adjust the path based on your folder structure
-import { IoMenu, IoClose } from "react-icons/io5";
+import who_we_are from "../assets/images/who_we_are.jpg";
+import Card from "../components/home/Card";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
 export default function Home() {
   return (
-    <div className="container mx-auto p-6">
+    <div className="w-full m-0 flex flex-col items-center">
       {/* Heading */}
-      <h1 className="text-3xl font-bold text-center mb-6">
-        {homeContent.heading}
-      </h1>
-      <h2 className="text-xl font-semibold text-center text-gray-700 mb-4">
-        {homeContent.subheading}
-      </h2>
-
-      <p className="text-center text-gray-600 mb-8">
-        {homeContent.introduction}
-      </p>
-
-      {/* Who We Are Section */}
-      <section className="mb-8">
-        <h3 className="text-2xl font-semibold mb-4">
-          {homeContent.whoWeAre.title}
+      <section className="flex flex-col gap-4 p-10 bg-white">
+        <h3 className="text-lg text-gray-500 font-semibold text-center uppercase tracking-wide">
+          {homeContent.heading}
         </h3>
-        <p className="text-gray-600">{homeContent.whoWeAre.description}</p>
+        <h1 className="text-6xl font-bold text-center text-gray-700 ">
+          <span className="text-primary">Innovating Healthcare for a</span>{" "}
+          <span className="text-secondary">Better Tomorrow</span>
+        </h1>
+
+        <p className="text-center text-primary text-lg">
+          {homeContent.introduction}
+        </p>
       </section>
 
-      {/* Vision and Mission Section */}
-      <section className="mb-8">
-        <h3 className="text-2xl font-semibold mb-4">
+      {/* Who We Are Section */}
+      <section className="w-[100%]  md:h-[500px] flex flex-col md:flex-row justify-between items-center bg-white mb-8">
+        <div className="md:w-[40%] pl-10 p-5 flex flex-col gap-4">
+          <h3 className="text-3xl font-semibold mb-4 text-primary">
+            {homeContent.whoWeAre.title}
+          </h3>
+          <p className=" text-primary text-lg">
+            {homeContent.whoWeAre.description}
+          </p>
+        </div>
+        <div className="md:w-[60%] h-[100%]">
+          <img
+            src={who_we_are}
+            alt=""
+            className="w-[100%] rounded-l-full h-[100%]"
+          />
+        </div>
+      </section>
+
+      {/* Vision Section */}
+      <section className="w-[100%] md:w-[95%] lg:w-[90%] xl:w-[80%] flex flex-col p-5 pb-10 justify-center items-center ">
+        <h3 className="text-3xl font-semibold mb-4 text-primary">
           {homeContent.visionMission.vision.title}
         </h3>
-        <p className="text-gray-600 mb-4">
+        <p className="text-primary text-md">
           {homeContent.visionMission.vision.description}
         </p>
+      </section>
 
-        <h3 className="text-2xl font-semibold mb-4">
-          {homeContent.visionMission.mission.title}
-        </h3>
-        <ul className="list-disc list-inside text-gray-600">
-          {homeContent.visionMission.mission.points.map((point, index) => (
-            <li key={index}>{point}</li>
-          ))}
-        </ul>
+      {/* Mission Section */}
+      <section className="w-full bg-secondary flex justify-center items-center">
+        <div className="w-[100%] md:w-[95%] lg:w-[90%] xl:w-[80%] flex flex-col items-center rounded-xl mb-8 p-5">
+          <h3 className="text-3xl font-semibold mb-4 text-white ">
+            {homeContent.visionMission.mission.title}
+          </h3>
+          {/* <ul className="list-inside grid grid-cols-1 md:grid-cols-3 gap-5">
+            {homeContent.visionMission.mission.points.map((point, index) => (
+              <li
+                key={index}
+                className="bg-white text-lg rounded-xl p-5"
+              >
+                {point}
+              </li>
+            ))}
+          </ul> */}
+          <ul className="list-inside">
+            {homeContent.visionMission.mission.points.map((point, index) => (
+              <li
+                key={index}
+                className="text-white font-semibold text-lg p-2 flex gap-3 items-center"
+              >
+                <div className="text-2xl">
+                  <IoIosArrowDroprightCircle />
+                </div>
+                <div>
+                  <p> {point}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </section>
 
       {/* Services Section */}
-      <section className="mb-8">
-        <h3 className="text-2xl font-semibold mb-4">
-          {homeContent.services.title}
-        </h3>
-        <ul className="list-disc list-inside text-gray-600">
-          {homeContent.services.items.map((service, index) => (
-            <li key={index} className="mb-2">
-              <strong>{service.title}:</strong> {service.description}
-            </li>
-          ))}
-        </ul>
+      <section className="w-full bg-white flex justify-center items-center">
+        <div className="w-[100%] md:w-[95%] lg:w-[90%] xl:w-[80%] flex flex-col items-center rounded-xl mb-8 p-5 ">
+          <h3 className="text-3xl font-semibold mb-4">
+            {homeContent.services.title}
+          </h3>
+          <div className="grid  grid-cols-1 md:grid-cols-2 grid-rows-1 md:grid-rows-2 gap-4">
+            {homeContent.services.items.map((service, index) => (
+              <Card
+                key={index}
+                title={service.title}
+                description={service.description}
+                className="mb-2"
+              />
+            ))}
+          </div>
+        </div>
       </section>
 
       {/* Why Choose Us Section */}
@@ -73,19 +118,19 @@ export default function Home() {
       </section>
 
       {/* Clients Section */}
-      <section className="mb-8">
-        <h3 className="text-2xl font-semibold mb-4">
+      <section className="mb-8 ">
+        <h3 className="text-2xl font-semibold mb-4 ">
           {homeContent.clients.title}
         </h3>
         <p className="text-gray-600">{homeContent.clients.description}</p>
       </section>
 
       {/* Call to Action Section */}
-      <section className="text-center mb-8">
-        <h3 className="text-2xl font-semibold mb-4">
+      <section className="text-center w-[100%]  bg-primary p-5">
+        <h3 className="text-3xl font-semibold mb-4 text-white">
           {homeContent.callToAction.title}
         </h3>
-        <p className="text-gray-600 mb-4">
+        <p className="mb-4 text-white">
           {homeContent.callToAction.description}
         </p>
       </section>
